1ed322a64b5f20eb54e1ae6977d7592611fe78fd
SGX <747166863@qq.com>
2016-12-22 14:40:03 +0800

    Ubuntu Code

---
+++ code/testoffice.py
@@ -0,0 +1,3 @@
+Dict = {
+'1': '1', '22': '22', '21': '21', '12': '12', '6': '6', '15': '15', 'cgPink': '10', 'fangtai': '27', 'PingGuoCu': '19', 'ccTablets': '26', 'other': '4', 'maotaiCircle': '3', 'cgBlue': '17', 'cMedicine': '0', 'jiuDiamond': '20', 'lanB': '2', 'lanA': '23', 'DouNaiYellow': '24', 'dessert2': '5', 'dessert3': '16', 'dessert1': '8', 'wMedicine': '9', 'dessert5': '7', 'PlantCreams': '18', 'cgCoffee': '14', 'zhuyan': '25', 'mask': '11', 'KouZiJiao': '13'
+}

ba2e9966ad6b5435c81637a6277aae38c222413b
sunguanxiong <747166863@qq.com>
2017-08-29 22:12:00 +0800

    done HG instructure

--- pycaffe_study/setLayers_HG.py
+++ pycaffe_study/setLayers_HG.py
@@ -10,12 +10,72 @@ from caffe import layers as L, params as P, to_proto
 from caffe.proto import caffe_pb2
 import caffe
 
-def conv_factory(bottom, ks, nout, stride=1, pad=0):
+def conv_bn_sc_rl(bottom, kSize, outCh, stride=1, pad=0):
+    conv;
+    
+    bn_scale(conv);
+    
+    relu;
+    
+    return relu
+
+def bn_scale() :
+    bn;
+    scale;
     return scale
 
-def conv :
-    
-    return
-
 def residual(bottom, numIn, numOut):
-    conv1 = conv_factory
+    bn_scale();
+    
+    conv_bn_sc_rl(); # 1x1
+    
+    conv_bn_sc_rl(); # 3x3
+    
+    conv1 = conv;
+    
+    if numIn == numOut:
+        skip = bottom;
+    else:
+        skip = conv(bottom); # 1x1
+    
+    concat(skip,conv1);
+    
+    return concat
+
+def hourglass(nRepeats, nFeatures, inputs):
+    up1 = residual(inputs)
+    low1 = pooling(bottom);
+    low1 = residual(low1);
+    
+    if nRepeats > 1:
+        low2 = hourglass(nRepeats-1, nFeatures, low1);
+    else:
+        low2 = residual(low1);
+    
+    low3 = residual(low2);
+    
+    up2 = upsample(low3);
+    
+    concat(up1,up2);
+        
+    return concat
+
+def lin(numIn, numOut, inputs):
+    return conv_bn_sc_rl(inputs, [1,1], numOut);
+
+der createModel():
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    

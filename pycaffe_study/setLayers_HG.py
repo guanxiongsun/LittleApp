22ba9df11b1fb13d35a99c2c3a05157bc7d56a55
sunguanxiong <747166863@qq.com>
2017-08-29 21:10:15 +0800

    Hourglasses Net with residual

---
+++ pycaffe_study/setLayers_HG.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 29 17:43:33 2017
+
+@author: sunguanxiong
+"""
+from __future__ import print_function
+from caffe import layers as L, params as P, to_proto
+from caffe.proto import caffe_pb2
+import caffe
+
+def conv_factory(bottom, ks, nout, stride=1, pad=0):
+    return scale
+
+def conv :
+    
+    return
+
+def residual(bottom, numIn, numOut):
+    conv1 = conv_factory
